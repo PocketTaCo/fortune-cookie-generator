ed7be0e2335b59408be1af01d690f5d2990e91e7
PocketTaCo <wizardrobe8@gmail.com>
2018-02-16 13:02:13 -1000

    work

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-function fortune-cookie-text() {
-	
-};
+var f = document.getElementById("fortune-cookie-text");
+document.getElementById("fortune-cookie-text").innerHTML = "the text" + f.innerHTML
 
+var FortuneList = ["You shall have the talent of doing absolutely nothing. ", 
+"Time shall stop in your vision at will. ", 
+"Gather the strength of all the omnipotent beings. ", 
+"gain nothing... thats it. ", "Love? nah you get none m8 outta here! "]
 
-function previous-fortunes() {
-	var text = ""
-};
