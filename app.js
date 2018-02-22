b077725d982b876318c2196895a9184b4716e2f3
PocketTaCo <wizardrobe8@gmail.com>
2018-02-21 14:47:10 -1000

    semi

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var f = document.getElementById("fortune-cookie-text");
-document.getElementById("fortune-cookie-text").innerHTML = "the text" + f.innerHTML
+//document.getElementById("fortune-cookie-text").innerHTML = generateFortuneCookie + f.innerHTML
 
 var FortuneList = ["You shall have the talent of doing absolutely nothing. ", 
-"Time shall stop in your vision at will. ", 
-"Gather the strength of all the omnipotent beings. ", 
-"gain nothing... thats it. ", "Love? nah you get none m8 outta here! "]
+						"Time shall stop in your vision at will. ", 
+						"Gather the strength of all the omnipotent beings. ", 
+						"gain nothing... thats it. ", "Love? nah you get none m8 outta here! "]
+
+
+function generateFortuneCookie (){
+	var randomFortune = Math.floor(Math.random() * Math.floor(FortuneList.length))
+		
+
+		f.innerHTML = FortuneList [randomFortune]
+}
 
