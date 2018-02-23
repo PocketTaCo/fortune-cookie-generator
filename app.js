bdc65d9fc54c48b2b84cb4e03b27dc8acefe6ff2
PocketTaCo <wizardrobe8@gmail.com>
2018-02-23 13:52:46 -1000

    done

--- app.js
+++ app.js
@@ -5,29 +5,30 @@ var FortuneList = ["You shall have the talent of doing absolutely nothing. ",
 						"Time shall stop in your vision at will. ", 
 						"Gather the strength of all the omnipotent beings. ", 
 						"gain nothing... thats it. ", "Love? nah you get none m8 outta here! ", "YOU'LL DIE FROM EPLIEPSY!!!!"]
-
 var PreviousFortune = []
 
-var prev = document.createElement("LI")
-document.getElementById("previous-fortunes").appendChild(prev)
-
 function generateFortuneCookie (){
-	var randomFortune = Math.floor(Math.random() * Math.floor(FortuneList.length))
+	var randomFortune = Math.floor(Math.random() * Math.floor(FortuneList.length));
 		
-
 		Fate.innerHTML = FortuneList [randomFortune]
 
-	
-
-
-	PreviousFortune.push(FortuneList[randomFortune])	
-	console.log(PreviousFortune)
-
-		if PreviousFortune  !== 1
+	PreviousFortune.push(FortuneList[randomFortune]);	
+	console.log(PreviousFortune);
 			
+	var prev = document.createElement("LI");
+	var TEXTME = document.createTextNode(FortuneList[randomFortune]);
+	prev.appendChild(TEXTME);
+		document.getElementById("previous-fortunes").appendChild(prev)
 
-			
-		PreviousFortune.push(Fate.innerHTML)
+	FortuneList.splice(randomFortune, 1);
+		while(FortuneList.length == 0){
+			FortuneList.push("You shall have the talent of doing absolutely nothing. ", 
+						"Time shall stop in your vision at will. ", 
+						"Gather the strength of all the omnipotent beings. ", 
+						"gain nothing... thats it. ", "Love? nah you get none m8 outta here! ", "YOU'LL DIE FROM EPLIEPSY!!!!")
+		}
 }
+
+
 
 	
