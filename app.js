817e402545c5a978c0e7b33b1906e676b5272730
PocketTaCo <wizardrobe8@gmail.com>
2018-02-22 23:07:54 -1000

    almost done

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-var f = document.getElementById("fortune-cookie-text");
+var Fate = document.getElementById("fortune-cookie-text");
 //document.getElementById("fortune-cookie-text").innerHTML = generateFortuneCookie + f.innerHTML
 
 var FortuneList = ["You shall have the talent of doing absolutely nothing. ", 
 						"Time shall stop in your vision at will. ", 
 						"Gather the strength of all the omnipotent beings. ", 
-						"gain nothing... thats it. ", "Love? nah you get none m8 outta here! "]
+						"gain nothing... thats it. ", "Love? nah you get none m8 outta here! ", "YOU'LL DIE FROM EPLIEPSY!!!!"]
 
+var PreviousFortune = []
+
+var prev = document.createElement("LI")
+document.getElementById("previous-fortunes").appendChild(prev)
 
 function generateFortuneCookie (){
 	var randomFortune = Math.floor(Math.random() * Math.floor(FortuneList.length))
 		
 
-		f.innerHTML = FortuneList [randomFortune]
+		Fate.innerHTML = FortuneList [randomFortune]
+
+	
+
+
+	PreviousFortune.push(FortuneList[randomFortune])	
+	console.log(PreviousFortune)
+
+		if PreviousFortune  !== 1
+			
+
+			
+		PreviousFortune.push(Fate.innerHTML)
 }
 
+	
